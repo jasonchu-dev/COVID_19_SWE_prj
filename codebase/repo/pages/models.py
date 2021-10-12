3f3802bad271ff40a2f8aa34b32e21a273b8410e
jasonchu-dev <chujason717@gmail.com>
2021-10-12 00:38:04 -0700

    trash json's

--- codebase/repo/pages/models.py
+++ codebase/repo/pages/models.py
@@ -4,40 +4,56 @@ from django.db import models
 
 # importing csv and json module
 import csv
-import json
-  
-# initializing the rows list
-rows = []
-  
+import json 
+
 # reading csv file
 with open("c19demographics.csv", "r") as csvfile:
     # creating a csv reader object
     csvreader = csv.reader(csvfile)
       
-    # extracting field names through first row
-    fields = next(csvreader)
-  
+    # initializing the info list
+    info = {"key": []} # problem here
+
+    # go to next line (skip titles)
+    next(csvreader)
+    
     # extracting each data row one by one
     for row in csvreader:
-        rows.append(row)
+        info["key"].append # problem here
+        (
+            {
+                "county": row[0],
+                "administered date": row[1]
+            }
+        )
+        #info.append(row)
 
 with open("c19demographics.json", "w") as jsonfile:
-    jsonfile.write(json.dumps(rows, indent=3))
+    jsonfile.write(json.dumps(info, indent=3))
 
-#clear rows for next set
-rows.clear()
+
 
 #reading other csv file
 with open("c19county.csv", "r") as csvfile:
     # creating a csv reader object
     csvreader = csv.reader(csvfile)
       
-    # extracting field names through first row
-    fields = next(csvreader)
+    #clear info for next set
+    info = {"key": []} # problem here
+
+    # go to next line (skip titles)
+    next(csvreader)
   
     # extracting each data row one by one
     for row in csvreader:
-        rows.append(row)
+        info["key"].append # problem here
+        (
+            {
+                "demographic category" : row[0],
+                "demographic value" : row[1]
+            }
+        )
+        #info.append(row)
 
 with open("c19county.json", "w") as jsonfile:
-    jsonfile.write(json.dumps(rows, indent=3))
+    jsonfile.write(json.dumps(info, indent=3))
