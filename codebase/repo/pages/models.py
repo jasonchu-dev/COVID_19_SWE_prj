ddf2e8d42711bd145219e7575c98e648ea26add1
jasonchu-dev <chujason717@gmail.com>
2021-10-12 01:39:34 -0700

    json is good

--- codebase/repo/pages/models.py
+++ codebase/repo/pages/models.py
@@ -9,51 +9,59 @@ import json
 # reading csv file
 with open("c19demographics.csv", "r") as csvfile:
     # creating a csv reader object
-    csvreader = csv.reader(csvfile)
+    csvreader = csv.DictReader(csvfile)
+
+    with open("c19demographics.json", "w") as jsonfile:
+        fieldnames = [
+            'demographic_category',
+            'demographic_value',
+            'administered_date',
+            'total_doses',
+            'cumulative_total_doses',
+            'pfizer_doses',
+            'cumulative_pfizer_doses',
+            'moderna_doses',
+            'cumulative_moderna_doses',
+            'jj_doses',
+            'cumulative_jj_doses',
+            'partially_vaccinated',
+            'total_partially_vaccinated',
+            'fully_vaccinated',
+            'cumulative_fully_vaccinated',
+            'at_least_one_dose',
+            'cumulative_at_least_one_dose'
+        ]
+        jsonwriter = csv.DictWriter(jsonfile, fieldnames=fieldnames, delimiter='\t')
+        
+        jsonwriter.writeheader()
+
+        for row in csvreader:
+            jsonwriter.writerow(row)
+
+
+
+# # old code
+# #reading other csv file
+# with open("c19county.csv", "r") as csvfile:
+#     # creating a csv reader object
+#     csvreader = csv.reader(csvfile)
       
-    # initializing the info list
-    info = {"key": []} # problem here
+#     #clear info for next set
+#     info = {"key": []} # problem here
 
-    # go to next line (skip titles)
-    next(csvreader)
-    
-    # extracting each data row one by one
-    for row in csvreader:
-        info["key"].append # problem here
-        (
-            {
-                "county": row[0],
-                "administered date": row[1]
-            }
-        )
-        #info.append(row)
-
-with open("c19demographics.json", "w") as jsonfile:
-    jsonfile.write(json.dumps(info, indent=3))
-
-
-
-#reading other csv file
-with open("c19county.csv", "r") as csvfile:
-    # creating a csv reader object
-    csvreader = csv.reader(csvfile)
-      
-    #clear info for next set
-    info = {"key": []} # problem here
-
-    # go to next line (skip titles)
-    next(csvreader)
+#     # go to next line (skip titles)
+#     next(csvreader)
   
-    # extracting each data row one by one
-    for row in csvreader:
-        info["key"].append # problem here
-        (
-            {
-                "demographic category" : row[0],
-                "demographic value" : row[1]
-            }
-        )
-        #info.append(row)
+#     # extracting each data row one by one
+#     for row in csvreader:
+#         info["key"].append # problem here
+#         (
+#             {
+#                 "demographic category" : row[0],
+#                 "demographic value" : row[1]
+#             }
+#         )
+#         #info.append(row)
 
-with open("c19county.json", "w") as jsonfile:
-    jsonfile.write(json.dumps(info, indent=3))
+# with open("c19county.json", "w") as jsonfile:
+#     jsonfile.write(json.dumps(info, indent=3))
