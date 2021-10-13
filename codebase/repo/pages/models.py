402cc9e266c57b8a82f376ba20ef5a61a194583f
Mahamadsaad Sheikh <mshei008@ucr.edu>
2021-10-12 22:41:54 -0700

    made models.py compatible

--- codebase/repo/pages/models.py
+++ codebase/repo/pages/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+import os
 
 # Create your models here.
 info = []
-with open ("demographics.csv", 'r') as csvfile:
+with open (os.path.join(os.path.dirname(os.path.realpath(__file__)), 'demographics.csv'), 'r') as csvfile:
     with open ("demographics.json", 'w') as jsonfile:
         for row in csvfile:
             info = row.split(',')
