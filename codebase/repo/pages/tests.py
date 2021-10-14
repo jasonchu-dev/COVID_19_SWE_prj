949335c136cb55b502974b1d49d481c6d49d1cf2
Mahamadsaad Sheikh <mshei008@ucr.edu>
2021-10-13 17:04:25 -0700

    added test cases in tests.py

--- codebase/repo/pages/tests.py
+++ codebase/repo/pages/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.http import HttpResponse
 
-# Create your tests here.
+
+class PagesTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+        
+    def test_response_error_handler(self):
+        return HttpResponse('Error handler content', status=403)
+        
