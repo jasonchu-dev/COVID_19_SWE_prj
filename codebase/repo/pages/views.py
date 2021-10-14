72d382802c9b88bf54d6299f50e6ec30de266b46
Brayan Montiel <bramontiel23@gmail.com>
2021-10-13 23:20:28 -0700

    updating views.py

--- codebase/repo/pages/views.py
+++ codebase/repo/pages/views.py
@@ -14,48 +14,28 @@ def results(request):
     s1 = request.GET['search']
     # Opening JSON file (CHANGE THIS ON LOCAL PC)
     #(CHANGE PATH ON LOCAL PC!!)
-    jsonFilePath = '/Users/brayanmontiel/Documents/UCR/2021--CURRENT/FALL 2021/CS180/codebase/project-main/cs180project-022-cs180ucrejects/codebase/repo/pages/demographics.json'
+    jsonFilePath = 'pages/demographics.json'
+    input = request.GET['search']
+    #function start
+    s2= ""
+    # Opening JSON file 
     f = open(jsonFilePath)
+    
     # data = JSON object as list
     data = json.load(f)
-    s2 = ''
+    
     # Iterating through the json
+    s2 = "demographic_category " + "demographic_value " + "administered_date " + "total_doses " + "cumulative_total_doses " + "pfizer_doses " + "cumulative_pfizer_doses " + "moderna_doses " + "cumulative_moderna_doses " + "jj_doses " + "cumulative_jj_doses " + "partially_vaccinated " + "total_partially_vaccinated " + "fully_vaccinated " + "cumulative_fully_vaccinated " + "at_least_one_dose "+ "cumulative_at_least_one_dose\n"
+
     for i in data:
-        if s1 == i['demographic_category']:
-            s2+= i['demographic_value']
-            s2+= ' '
-            s2+= i['administered_date']
-            s2+= ' '
-            s2+= str(i['total_doses'])
-            s2+= ' '
-            s2+= str(i['cumulative_total_doses'])
-            s2+= ' '
-            s2+= str(i['pfizer_doses'])
-            s2+= ' '
-            s2+= str(i['cumulative_pfizer_doses'])
-            s2+= ' '
-            s2+= str(i['moderna_doses'])
-            s2+= ' '
-            s2+= str(i['cumulative_moderna_doses'])
-            s2+= ' '
-            s2+= str(i['jj_doses'])
-            s2+= ' '
-            s2+= str(i['cumulative_jj_doses'])
-            s2+= ' '
-            s2+= str(i['partially_vaccinated'])
-            s2+= ' '
-            s2+= str(i['total_partially_vaccinated'])
-            s2+= ' '
-            s2+= str(i['fully_vaccinated'])
-            s2+= ' '
-            s2+= str(i['cumulative_fully_vaccinated'])
-            s2+= ' '
-            s2+= str(i['at_least_one_dose'])
-            s2+= ' '
-            s2+= str(i['cumulative_at_least_one_dose'])
-            s2+= '\n'
-        else:
-            s2 = 'No results...try again.'
+        if(input == i['demographic_value']):
+            s2 = s2 + i['demographic_category'] + i['demographic_value'] + i['administered_date'] + str(i['total_doses']) + str(i['cumulative_total_doses']) + str(i['pfizer_doses']) + str(i['cumulative_pfizer_doses']) + str(i['moderna_doses']) + str(i['cumulative_moderna_doses']) + str(i['jj_doses']) + str(i['cumulative_jj_doses']) + str(i['partially_vaccinated']) + str(i['total_partially_vaccinated']) + str(i['fully_vaccinated']) + str(i['cumulative_fully_vaccinated']) + str(i['at_least_one_dose']) + str(i['cumulative_at_least_one_dose']) + '\n'
+        elif(input == i['administered_date']):
+            s2 = s2 + i['demographic_category'] + i['demographic_value'] + i['administered_date'] + str(i['total_doses']) + str(i['cumulative_total_doses']) + str(i['pfizer_doses']) + str(i['cumulative_pfizer_doses']) + str(i['moderna_doses']) + str(i['cumulative_moderna_doses']) + str(i['jj_doses']) + str(i['cumulative_jj_doses']) + str(i['partially_vaccinated']) + str(i['total_partially_vaccinated']) + str(i['fully_vaccinated']) + str(i['cumulative_fully_vaccinated']) + str(i['at_least_one_dose']) + str(i['cumulative_at_least_one_dose']) + '\n'
+        elif(input == i['demographic_category']):
+            s2 = s2 + i['demographic_category'] + i['demographic_value'] + i['administered_date'] + str(i['total_doses']) + str(i['cumulative_total_doses']) + str(i['pfizer_doses']) + str(i['cumulative_pfizer_doses']) + str(i['moderna_doses']) + str(i['cumulative_moderna_doses']) + str(i['jj_doses']) + str(i['cumulative_jj_doses']) + str(i['partially_vaccinated']) + str(i['total_partially_vaccinated']) + str(i['fully_vaccinated']) + str(i['cumulative_fully_vaccinated']) + str(i['at_least_one_dose']) + str(i['cumulative_at_least_one_dose']) + '\n'
+        #else:
+         #   s2 = "nothing found"
     # Closing file
     f.close()
     return render(request,'results.html',{'search':s2})
