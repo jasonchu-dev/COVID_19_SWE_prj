0e87727b2d32a172fa2626a87504ac067bdda2cc
Brayan Montiel <bramontiel23@gmail.com>
2021-10-18 23:41:56 -0700

    adding delete.html - still need to finish, won't work completely.

--- codebase/repo/pages/urls.py
+++ codebase/repo/pages/urls.py
@@ -1,10 +1,12 @@
 # pages/urls.py
 from django.urls import path
 from . import views
-from .views import HomePageView, AboutPageView # new
+from .views import HomePageView, AboutPageView, DeletePageView  # new
 
 urlpatterns = [
     path('about/', AboutPageView.as_view(), name='about'), 
     path('', HomePageView.as_view(), name='home'),  
     path('search/', views.results, name='search'),
+    path('delete/', DeletePageView.as_view(), name='delete'),
+    path('delete/', views.delete_record, name='delete'),
 ]
--- codebase/repo/pages/views.py
+++ codebase/repo/pages/views.py
@@ -4,38 +4,48 @@ from django.shortcuts import render
 import requests
 import json
 
+#Load json into array data
+jsonFilePath = 'pages/demographics.json'
+f = open(jsonFilePath)
+#elements of json file will be in array: data#
+data = json.load(f)
+f.close()
+
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
 class AboutPageView(TemplateView): 
     template_name = 'about.html'
 
-def results(request):
-    s1 = request.GET['search']
-    # Opening JSON file (CHANGE THIS ON LOCAL PC)
-    #(CHANGE PATH ON LOCAL PC!!)
-    jsonFilePath = 'pages/demographics.json'
-    input = request.GET['search']
-    #function start
-    s2= ""
-    # Opening JSON file 
-    f = open(jsonFilePath)
-    
-    # data = JSON object as list
-    data = json.load(f)
-    
-    # Iterating through the json
-    s2 = "demographic_category " + "demographic_value " + "administered_date " + "total_doses " + "cumulative_total_doses " + "pfizer_doses " + "cumulative_pfizer_doses " + "moderna_doses " + "cumulative_moderna_doses " + "jj_doses " + "cumulative_jj_doses " + "partially_vaccinated " + "total_partially_vaccinated " + "fully_vaccinated " + "cumulative_fully_vaccinated " + "at_least_one_dose "+ "cumulative_at_least_one_dose\n"
+class DeletePageView(TemplateView): 
+    template_name = 'delete.html'
 
+#search method
+def results(request):
+    input = request.GET['search'] #retrieves the GET for search
+    s2= "" #will be our return string
     for i in data:
-        if(input == i['demographic_value']):
-            s2 = s2 + i['demographic_category'] + i['demographic_value'] + i['administered_date'] + str(i['total_doses']) + str(i['cumulative_total_doses']) + str(i['pfizer_doses']) + str(i['cumulative_pfizer_doses']) + str(i['moderna_doses']) + str(i['cumulative_moderna_doses']) + str(i['jj_doses']) + str(i['cumulative_jj_doses']) + str(i['partially_vaccinated']) + str(i['total_partially_vaccinated']) + str(i['fully_vaccinated']) + str(i['cumulative_fully_vaccinated']) + str(i['at_least_one_dose']) + str(i['cumulative_at_least_one_dose']) + '\n'
-        elif(input == i['administered_date']):
-            s2 = s2 + i['demographic_category'] + i['demographic_value'] + i['administered_date'] + str(i['total_doses']) + str(i['cumulative_total_doses']) + str(i['pfizer_doses']) + str(i['cumulative_pfizer_doses']) + str(i['moderna_doses']) + str(i['cumulative_moderna_doses']) + str(i['jj_doses']) + str(i['cumulative_jj_doses']) + str(i['partially_vaccinated']) + str(i['total_partially_vaccinated']) + str(i['fully_vaccinated']) + str(i['cumulative_fully_vaccinated']) + str(i['at_least_one_dose']) + str(i['cumulative_at_least_one_dose']) + '\n'
-        elif(input == i['demographic_category']):
-            s2 = s2 + i['demographic_category'] + i['demographic_value'] + i['administered_date'] + str(i['total_doses']) + str(i['cumulative_total_doses']) + str(i['pfizer_doses']) + str(i['cumulative_pfizer_doses']) + str(i['moderna_doses']) + str(i['cumulative_moderna_doses']) + str(i['jj_doses']) + str(i['cumulative_jj_doses']) + str(i['partially_vaccinated']) + str(i['total_partially_vaccinated']) + str(i['fully_vaccinated']) + str(i['cumulative_fully_vaccinated']) + str(i['at_least_one_dose']) + str(i['cumulative_at_least_one_dose']) + '\n'
-        #else:
-         #   s2 = "nothing found"
-    # Closing file
-    f.close()
+        if input == i['demographic_value'] :
+            s2 = s2 + str(i['ID']) +' | '+ i['demographic_category'] +' | '+ i['demographic_value'] +' | '+ i['administered_date'] +' | '+ str(i['total_doses']) +' | '+ str(i['cumulative_total_doses']) +' | '+ str(i['pfizer_doses']) +' | '+ str(i['cumulative_pfizer_doses']) +' | '+ str(i['moderna_doses']) +' | '+ str(i['cumulative_moderna_doses']) +' | '+ str(i['jj_doses']) +' | '+ str(i['cumulative_jj_doses']) +' | '+ str(i['partially_vaccinated']) +' | '+ str(i['total_partially_vaccinated']) +' | '+ str(i['fully_vaccinated']) +' | '+ str(i['cumulative_fully_vaccinated']) +' | '+ str(i['at_least_one_dose']) +' | '+ str(i['cumulative_at_least_one_dose']) + '\n'
+        elif input == i['administered_date'] :
+            s2 = s2 + str(i['ID']) +' | '+ i['demographic_category'] +' | '+ i['demographic_value'] +' | '+ i['administered_date'] +' | '+ str(i['total_doses']) +' | '+ str(i['cumulative_total_doses']) +' | '+ str(i['pfizer_doses']) +' | '+ str(i['cumulative_pfizer_doses']) +' | '+ str(i['moderna_doses']) +' | '+ str(i['cumulative_moderna_doses']) +' | '+ str(i['jj_doses']) +' | '+ str(i['cumulative_jj_doses']) +' | '+ str(i['partially_vaccinated']) +' | '+ str(i['total_partially_vaccinated']) +' | '+ str(i['fully_vaccinated']) +' | '+ str(i['cumulative_fully_vaccinated']) +' | '+ str(i['at_least_one_dose']) +' | '+ str(i['cumulative_at_least_one_dose']) + '\n'
+        elif input == i['demographic_category'] :
+            s2 = s2 + str(i['ID']) +' | '+ i['demographic_category'] +' | '+ i['demographic_value'] +' | '+ i['administered_date'] +' | '+ str(i['total_doses']) +' | '+ str(i['cumulative_total_doses']) +' | '+ str(i['pfizer_doses']) +' | '+ str(i['cumulative_pfizer_doses']) +' | '+ str(i['moderna_doses']) +' | '+ str(i['cumulative_moderna_doses']) +' | '+ str(i['jj_doses']) +' | '+ str(i['cumulative_jj_doses']) +' | '+ str(i['partially_vaccinated']) +' | '+ str(i['total_partially_vaccinated']) +' | '+ str(i['fully_vaccinated']) +' | '+ str(i['cumulative_fully_vaccinated']) +' | '+ str(i['at_least_one_dose']) +' | '+ str(i['cumulative_at_least_one_dose']) + '\n'
+        elif input == str(i['ID']) :
+            s2 = s2 + str(i['ID']) +' | '+ i['demographic_category'] +' | '+ i['demographic_value'] +' | '+ i['administered_date'] +' | '+ str(i['total_doses']) +' | '+ str(i['cumulative_total_doses']) +' | '+ str(i['pfizer_doses']) +' | '+ str(i['cumulative_pfizer_doses']) +' | '+ str(i['moderna_doses']) +' | '+ str(i['cumulative_moderna_doses']) +' | '+ str(i['jj_doses']) +' | '+ str(i['cumulative_jj_doses']) +' | '+ str(i['partially_vaccinated']) +' | '+ str(i['total_partially_vaccinated']) +' | '+ str(i['fully_vaccinated']) +' | '+ str(i['cumulative_fully_vaccinated']) +' | '+ str(i['at_least_one_dose']) +' | '+ str(i['cumulative_at_least_one_dose']) + '\n'
+        #else: 
+        #    s2 = "No matching results...pleast try again."
     return render(request,'results.html',{'search':s2})
+
+#delte method
+def delete_record(request):
+    input = request.GET['delete'] #retrieves the GET for deleting
+    s3= '' #will be our return string
+    for i in data:
+        if input == i['ID'] :
+            del data[i]
+            s3 = 'Success. Entry ' + input + ' has been deleted.'
+        else:
+            s3 = 'Record not found. Please try again.'
+        #    s2 = "No matching results...pleast try again."
+    return render(request,'delete.html',{'delete':s3})
