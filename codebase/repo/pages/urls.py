e847dd78c777f46e670dc7cfeb95a8cf4e34a25e
Alan Z <59679887+hongan-z@users.noreply.github.com>
2021-11-09 15:08:54 -0800

    added pageViews about increment analytics

--- codebase/repo/pages/urls.py
+++ codebase/repo/pages/urls.py
@@ -1,10 +1,14 @@
 # pages/urls.py
 from django.urls import path
 from . import views
-from .views import HomePageView, AboutPageView # new
+from .views import HomePageView, AboutPageView ,IncrementPageView # new
+# new
 
+from pages.views import increment_analytics
 urlpatterns = [
     path('about/', AboutPageView.as_view(), name='about'), 
     path('', HomePageView.as_view(), name='home'),  
     path('search/', views.results, name='search'),
+    path('Increment/', IncrementPageView.as_view(), name='increment'),
+    path('Increment/monthlyIncrement', views.increment_analytics , name='increment')
 ]
